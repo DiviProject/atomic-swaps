8bd4b9dfe94398cb0275dd8a66f980195a6d813a
Chris Cates <hello@chriscates.ca>
2019-12-06 18:12:22 -0500

    Updates

---
+++ precache-manifest.549b470d9823ca6da53e95a10d1cc953.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdbfe15ac87275c7c565deec211deae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2080ec1028fe544d3f2d",
+    "url": "/static/css/main.6961a94c.chunk.css"
+  },
+  {
+    "revision": "43df20b1d6f684ab4eb3",
+    "url": "/static/js/2.87cc141c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.87cc141c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2080ec1028fe544d3f2d",
+    "url": "/static/js/main.a3693603.chunk.js"
+  },
+  {
+    "revision": "b858bb1a7f4a47162361",
+    "url": "/static/js/runtime-main.485c14cf.js"
+  },
+  {
+    "revision": "a552edcdd0b2c25ee26fe0d1fb222761",
+    "url": "/static/media/bitcoin.a552edcd.png"
+  }
+]);
